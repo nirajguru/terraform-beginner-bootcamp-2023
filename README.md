265584d06f76202a2c4526e109ac2e4859b42c55
Niraj G <niraj.guru@gmail.com>
2023-10-01 01:32:44 +0000

    Adding random provider

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Terraform Beginner Bootcamp 2023
 
+## Providers
+are plugins that help Terraform talk to different platforms, cloud providers. Each provider has a list of resources that terraform can manage
--- main.tf
+++ main.tf
@@ -1,1 +1,14 @@
+terraform {
+  required_providers {
+    random = {
+      source = "hashicorp/random"
+      version = "3.5.1"
+    }
+  }
+}
 
+resource "random_string" "random" {
+  length           = 16
+  special          = true
+  override_special = "/@£$"
+}
