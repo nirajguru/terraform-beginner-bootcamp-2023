3fb13cd282c5f6c40df53c902f9eff89114e32f4
Niraj Gurupanchayan <niraj.guru@gmail.com>
2023-10-22 00:10:01 +0100

    Refactored s3 bucket in a module

---
+++ modules/terrahouse_aws/main.tf
@@ -0,0 +1,35 @@
+terraform {
+ 
+  required_providers {
+    random = {
+      source  = "hashicorp/random"
+      version = "3.5.1"
+    }
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "random_string" "bucket_name" {
+  # Bucket naming rules https://docs.aws.amazon.com/AmazonS3/latest/userguide/bucketnamingrules.html
+  length  = 16
+  special = false
+  lower   = true
+  upper   = false
+}
+
+
+resource "aws_s3_bucket" "my_bucket" {
+  bucket = random_string.bucket_name.id
+
+  tags = {
+    UserUuid = var.user_uuid
+  }
+}
+
