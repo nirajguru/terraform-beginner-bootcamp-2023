cd4da8cf8ee2e9b1b04d4bd494e0f2e01e412bce
Niraj Gurupanchayan <niraj.guru@gmail.com>
2023-10-22 23:25:24 +0100

    Added S3 bucket for website hosting

--- modules/terrahouse_aws/outputs.tf
+++ modules/terrahouse_aws/outputs.tf
@@ -1,3 +1,7 @@
-output "random_bucket_name" {
+output "bucket_name" {
   value = random_string.bucket_name.id
+}
+
+output "s3_website_url" {
+  value = aws_s3_bucket_website_configuration.s3_website.website_endpoint
 }
