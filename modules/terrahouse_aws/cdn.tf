68c0073977d1f4c86b3f6f1d6a82a703b0c40ab0
Niraj Gurupanchayan <87394957+nirajguru@users.noreply.github.com>
2023-10-24 18:40:34 +0100

    Added block to invalidate Cloudfront cache (#17)

--- modules/terrahouse_aws/cdn.tf
+++ modules/terrahouse_aws/cdn.tf
@@ -97,3 +97,16 @@ resource "aws_cloudfront_origin_access_control" "default" {
   signing_behavior                  = "always"
   signing_protocol                  = "sigv4"
 }
+
+resource "terraform_data" "invalidate_cache" {
+  triggers_replace = terraform_data.content_version.output
+
+  # https://developer.hashicorp.com/terraform/language/expressions/strings#heredoc-strings
+  provisioner "local-exec" {
+    command = "aws --version"
+    # command = <<COMMAND
+    # aws cloudfront create-invalidation --distribution-id ${aws_cloudfront_distribution.s3_distribution.id} \
+    # --paths "/*"
+    # COMMAND
+  }
+}
