efdbd347e4cd2a598a5c5f42a0ab73b2e8ad9c83
Niraj Gurupanchayan <niraj.guru@gmail.com>
2023-10-24 22:49:41 +0100

    Assets upload using for_each

--- modules/terrahouse_aws/storage.tf
+++ modules/terrahouse_aws/storage.tf
@@ -48,8 +48,22 @@ resource "aws_s3_object" "error_file" {
     ignore_changes = [ etag ]
     replace_triggered_by = [ terraform_data.content_version.output ]
   }
-
 }
+
+resource "aws_s3_object" "upload_assets" {
+
+  for_each = fileset("${path.root}/public/assets","*.{jpg}")
+  bucket = aws_s3_bucket.my_bucket.bucket
+  key    = "assets/${each.key}"
+  source = "${path.root}/public/assets/${each.key}"
+  etag   = filemd5("${path.root}/public/assets/${each.key}")
+  content_type = "text/html"
+  lifecycle {
+    ignore_changes = [ etag ]
+    replace_triggered_by = [ terraform_data.content_version.output ]
+  }
+}
+
 
 resource "aws_s3_bucket_policy" "my_bucket_policy" {
   bucket = aws_s3_bucket.my_bucket.id
